946dfc8d19acb72c6e88bd8f9966899ad1b6f7eb
msvonderheid <89092376+msvonderheid@users.noreply.github.com>
2022-03-10 11:59:23 -0600

    Update main.swift

--- Arrays Lab #1 - New/main.swift
+++ Arrays Lab #1 - New/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+//1. Declare an array called names of type string.  Initialize the array with the following names: Abraham, Bill, George, Theodore, Barack.
+
+
+
+
+
+//2. Using an index reference, print the name Barack
+
+
+
+
+
+//3. Using an index reference, print the name Abraham
+
+
+
+
+
+//4. Use a for in loop with the names.count attribute to print all of the names in the array on a new line
+
+
+
+
+
+//5. Add the name Joe to your list using the .append method (you may need to google this)
+
+
+
+
+
+//6. Use the insert(_:at:) method to add Franklin into the array as the second element. Print the contents of the collection
 
